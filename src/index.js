c99221f78459e3501ce06065eaf06f66c41c3c94
Pedro Lucas <pedrolucas1101@proton.me>
2023-10-06 14:13:44 -0300

    cooomitei

--- src/index.js
+++ src/index.js
@@ -1,10 +1,24 @@
 import { config } from "dotenv";
-import { Client } from "discord.js";
+import { Client, GatewayIntentBits } from "discord.js";
 
 config();
 
-const client = new Client({intents: ['Guilds', 'GuildMessages']});
+const client = new Client({intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent
+]});
+
 const TOKEN = process.env.TOKEN;
 
 
 client.login(TOKEN);
+
+client.on('ready', () => {
+    console.log(`${client.user.tag} pronto!`);
+});
+
+client.on('messageCreate', async (message) => {
+    var date = new Date(message.createdTimestamp);
+    console.log(`${message.author.username}: ${message.content} ||| DATA: ${date.getDate()}/${date.getMonth()+1}/${date.getFullYear()} ||| HORA: ${date.getHours()}:${date.getHours()}:${date.getSeconds()} ||| CANAL: ${message.channel.name} `)
+});
